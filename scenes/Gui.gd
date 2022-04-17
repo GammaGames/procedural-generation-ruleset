2dda19a360c97269cc9a056653aa80534f646550
GammaGames <jesse007@ymail.com>
2022-04-17 00:55:26 -0600

    update yaml asset for web export, tweak some settings

--- scenes/Gui.gd
+++ scenes/Gui.gd
@@ -15,48 +15,50 @@ signal zoom
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    generate_button.connect("pressed", self, "_pressed")
-    zoom_slider.connect("value_changed", self, "_value_changed")
+	generate_button.connect("pressed", self, "_pressed")
+	zoom_slider.connect("value_changed", self, "_value_changed")
 
 
 func _pressed():
-    load_config()
+	load_config()
+	seeed.release_focus()
+	variance.release_focus()
+	textbox.release_focus()
 
 
 func _value_changed(value):
-    emit_signal("zoom", value)
+	emit_signal("zoom", value)
 
 
 func load_config():
-    var sed = seeed.text
-    print(sed)
-    if sed != "":
-        if int(sed) != 0:
-            sed = int(sed)
-        else:
-            sed = sed.hash()
-    else:
-        sed = null
+	var sed = seeed.text
+	if sed != "":
+		if int(sed) != 0:
+			sed = int(sed)
+		else:
+			sed = sed.hash()
+	else:
+		sed = null
 
-    if tilegen.parse_config(textbox.text, sed):
-        error_label.visible = false
-        set_textures()
-        emit_signal("loaded")
-    else:
-        error_label.visible = true
+	if tilegen.parse_config(textbox.text, sed):
+		error_label.visible = false
+		set_textures()
+		emit_signal("loaded")
+	else:
+		error_label.visible = true
 
 
 func set_textures():
-    for child in texture_stack.get_children():
-        child.queue_free()
-    for generator in tilegen.generators:
-        var gen = tilegen.generators[generator]
-        var new_box = texture_box.duplicate()
-        var tex = NoiseTexture.new()
-        tex.width = 128
-        tex.height = 128
-        tex.noise = gen
-        new_box.get_node("Texture").texture = tex
-        new_box.get_node("Label").text = generator
-        texture_stack.add_child(new_box)
-        new_box.visible = true
+	for child in texture_stack.get_children():
+		child.queue_free()
+	for generator in tilegen.generators:
+		var gen = tilegen.generators[generator]
+		var new_box = texture_box.duplicate()
+		var tex = NoiseTexture.new()
+		tex.width = 128
+		tex.height = 128
+		tex.noise = gen
+		new_box.get_node("Texture").texture = tex
+		new_box.get_node("Label").text = generator
+		texture_stack.add_child(new_box)
+		new_box.visible = true
